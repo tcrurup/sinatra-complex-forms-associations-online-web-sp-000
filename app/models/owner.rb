d970533501ad84844e4cfbaaeb96ab54ad26a7f2
Anthony Rurup <tcrurup@gmail.com>
2019-06-24 18:02:59 +0000

    Automatically backed up by Learn

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,3 +1,10 @@
 class Owner < ActiveRecord::Base
   has_many :pets
+  
+  def show_pets_as_string
+    str = String.new
+    self.pets.each do |pet|
+      str += "|  #{pet.name}"
+    end
+  end
 end
