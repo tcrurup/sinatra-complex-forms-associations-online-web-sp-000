b0ff7caeee65b7c6912ba0873f65090649b982f5
Anthony Rurup <tcrurup@gmail.com>
2019-06-24 18:05:19 +0000

    Automatically backed up by Learn

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -2,9 +2,10 @@ class Owner < ActiveRecord::Base
   has_many :pets
   
   def show_pets_as_string
-    str = String.new
+    str = ""
     self.pets.each do |pet|
-      str += "|  #{pet.name}  "
+      str += '|' unless str == ""
+      str += " #{pet.name} "
     end
     str
   end
